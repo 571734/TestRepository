3f3fff6ea0dc8bf347514e1b5d031fa48fa5781e
571734 <571734@stud.hvl.no>
2019-01-23 10:41:40 +0100

    test

---
+++ src/randomPackage/RandomClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package randomPackage;
+
+/**
+ * @author Sivert
+ *
+ */
+public class RandomClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
